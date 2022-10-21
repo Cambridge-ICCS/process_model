7e4f46da820943e54b60a8bf55b1469f8e5164b6
Simon Clifford <sjc306@cam.ac.uk>
2022-10-21 18:53:19 +0100

    Use click.CliRunner rather than subprocess

--- tests/test_process_model.py
+++ tests/test_process_model.py
@@ -1,38 +1,58 @@
-import subprocess
 import os
+from click.testing import CliRunner
+from process_model import main
+
+
+def test_cli_works(tmp_path):
+    """
+    Test if process_model generates the Fortran file.
+    """
+    runner = CliRunner()
+    result = runner.invoke(main)
+    assert result.exit_code != 0
+    result = runner.invoke(main, ['--help'])
+    assert result.exit_code == 0
+    assert 'Usage: main [OPTIONS] MODEL_DIR...' in result.output
 
 
 def test_f90_created(tmp_path):
     """
     Test if process_model generates the Fortran file.
     """
-    subprocess.run(["process_model", "tests/my_model/", "-o",
-                    tmp_path / "testf.f90"], check=True)
-    assert os.path.isfile(tmp_path / "testf.f90")
+    runner = CliRunner()
+    output_path = tmp_path / 'testf.f90'
+    result = runner.invoke(main, ['tests/my_model', '-o', output_path])
+    assert result.exit_code == 0
+    assert os.path.isfile(output_path)
 
 
 def test_f90_tags_arg(tmp_path):
     """
     Test if process_model accepts tags argument correctly.
     """
-    ret = subprocess.run(["process_model", "-t", "unknown", "tests/my_model/",
-                          "-o", tmp_path / "testf.f90"])
-    assert not os.path.isfile(tmp_path / "testf.f90")
-    assert ret.returncode == 1
-    subprocess.run(["process_model", "-t", "serve", "tests/my_model/",
-                    "-o", tmp_path / "testf.f90"], check=True)
-    assert os.path.isfile(tmp_path / "testf.f90")
+    runner = CliRunner()
+    output_path = tmp_path / 'testf.f90'
+    result = runner.invoke(main, ['tests/my_model', '-t', 'unknown',
+                                  '-o', output_path])
+    assert not os.path.isfile(output_path)
+    assert result.exit_code == 1
+    result = runner.invoke(main, ['tests/my_model', '-t', 'serve',
+                                  '-o', output_path])
+    assert result.exit_code == 0
+    assert os.path.isfile(output_path)
 
 
 def test_f90_sig_def(tmp_path):
     """
     Test if process_model accepts signature_def argument correctly.
     """
-    ret = subprocess.run(["process_model", "-s", "unknown", "tests/my_model/",
-                          "-o", tmp_path / "testf.f90"])
-    assert ret.returncode == 1
-    assert not os.path.isfile(tmp_path / "testf.f90")
-    subprocess.run(["process_model", "-s", "serving_default",
-                    "tests/my_model/", "-o", tmp_path / "testf.f90"],
-                   check=True)
-    assert os.path.isfile(tmp_path / "testf.f90")
+    runner = CliRunner()
+    output_path = tmp_path / 'testf.f90'
+    result = runner.invoke(main, ['tests/my_model', '-s', 'unknown',
+                                  '-o', output_path])
+    assert not os.path.isfile(output_path)
+    assert result.exit_code == 1
+    result = runner.invoke(main, ['tests/my_model', '-s', 'serving_default',
+                                  '-o', output_path])
+    assert os.path.isfile(output_path)
+    assert result.exit_code == 0
